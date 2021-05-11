67968cb6058674fca004f3cc8273581bab1523da
kevinpitstopproductions <kevin.doran@pitstopproductions.co.uk>
2021-05-11 12:16:16 +0100

    Create PSP_Package.lua

---
+++ PSP_Package.lua
@@ -0,0 +1,21 @@
+--[[
+ ReaScript Name: PSP Scripts
+ MetaPackage: true
+ Author: kevin@pitstop
+ REAPER: 6+
+ Extensions: SWS
+ Version: 1.0
+ Provides:
+  [Main] PSP_Create Regions from Selected Item Source Destinations.lua
+  [Main] PSP_Offset Source Start to 50% Item Length.lua
+
+ About:
+  A collection of my reaper workflow scripts
+--[[
+	
+	
+	
+--[[
+ * Changelog:
+   * v1.0 (2021-05-11)
+]]
