5027c665ac0dd2662d3edd2687da015127c8a81a
kevinpitstopproductions <83832967+kevinpitstopproductions@users.noreply.github.com>
2021-07-09 13:30:28 +0100

    Update PSP_Create Razor Edit Regions From Parent Items.lua

--- Scripts/PSP_Create Razor Edit Regions From Parent Items.lua
+++ Scripts/PSP_Create Razor Edit Regions From Parent Items.lua
@@ -3,23 +3,18 @@
  * Author: GU-on
  * Licence: GPL v3
  * REAPER: 6.31
- * Version: 1.0
+ * Version: 1.1
 --]]
 
 --[[
  * Changelog:
- * v1.0 (2021-07-08)
+ * v1.1 (2021-07-09)
  	+ Initial Release
 --]]
 
---[[
-P_RAZOREDITS : const char * : list of razor edit areas, as space-separated triples of start time, end time, and envelope GUID string.
-Example: "0.00 1.00 \"\" 0.00 1.00 "{xyz-...}"
-]]
-
 --- DEBUG ---
 
-local console = true
+local console = false
 
 local function Msg(text) if console then reaper.ShowConsoleMsg(tostring(text) .. '\n') end end
 
@@ -29,39 +24,55 @@ local re_table = {}
 
 --- FUNCTIONS ---
 
-local function GetGUID(track, envidx)
-    local _, guid = reaper.GetSetEnvelopeInfo_String(reaper.GetTrackEnvelope(track, envidx), "GUID", "", false) return guid end
-
 --- MAIN ---
 
-local function Main(re_table)
-    for i = 0, reaper.CountSelectedMediaItems(0) - 1 do
-        local item = reaper.GetSelectedMediaItem(0, i)
-        local track = reaper.GetMediaItemTrack(item)
-        local track_index = reaper.GetMediaTrackInfo_Value(track, "IP_TRACKNUMBER")
-        local track_depth = 1
-        local top_track_depth = reaper.GetTrackDepth(track)
-        local item_start = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
-        local item_end = reaper.GetMediaItemInfo_Value(item, "D_LENGTH") + item_start
+reaper.ClearConsole()
 
-        for e=0, reaper.CountTrackEnvelopes(track)-1 do
-            re_table[track] = (re_table[track] or "") .. string.format([[%.16f %.16f "%s" ]], item_start, item_end, GetGUID(track, e)) end
+for i = 0, reaper.CountSelectedMediaItems(0)-1 do
+    local item = reaper.GetSelectedMediaItem(0, i)
+    local track = reaper.GetMediaItemTrack(item)
+    local track_index = reaper.GetMediaTrackInfo_Value(track, "IP_TRACKNUMBER")-1
+    local item_start = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
+    local item_end = reaper.GetMediaItemInfo_Value(item, "D_LENGTH") + item_start
+    local index = track_index
 
-        while (track_depth > top_track_depth) and (track_index < reaper.CountTracks(0)) do
-            track_index = track_index + 1
-            track_depth = reaper.GetTrackDepth(reaper.GetTrack(0, track_index))
+    Msg("total tracks " .. reaper.CountTracks(0) .. "\n")
+    Msg("Selected item is on track " .. string.format("%d",track_index))
+
+    if reaper.GetTrack(0, track_index+1) then -- if item not on last track
+        Msg("track exists after current track")
+        Msg("current track depth is " .. reaper.GetTrackDepth(track))
+
+        for j = track_index, reaper.CountTracks(0)-1 do
+            if reaper.GetTrackDepth(reaper.GetTrack(0, j+1)) <= reaper.GetTrackDepth(track) then break end
+            index = index + 1
         end
 
-        for j=reaper.GetMediaTrackInfo_Value(track, "IP_TRACKNUMBER"), track_index do
-            track = reaper.GetTrack(0, j-1)
+        Msg("last folder item for track is " .. string.format("%d",index))
+
+        for j = track_index, index do
+            Msg("add razor edits to " .. string.format("%d",j))
+            track = reaper.GetTrack(0, j)
+
+            local env_count = reaper.CountTrackEnvelopes(track)
+            if env_count > 0 then
+                Msg("track " .. string.format("%d",j) .. " has " .. env_count .. " envelopes")
+                for e = 0, env_count-1 do
+                    local env = reaper.GetTrackEnvelope(track, e)
+                    local _, guid = reaper.GetSetEnvelopeInfo_String(env, "GUID", "", false)
+                    re_table[track] = (re_table[track] or "") .. string.format([[%.16f %.16f "%s" ]], item_start, item_end, guid) 
+                end
+            end
+
             re_table[track] = (re_table[track] or "") .. string.format([[%.16f %.16f "" ]], item_start, item_end) 
         end
+    else
+        re_table[track] = (re_table[track] or "") .. string.format([[%.16f %.16f "" ]], item_start, item_end) 
     end
 
     for track, str in pairs(re_table) do
         reaper.GetSetMediaTrackInfo_String( track, "P_RAZOREDITS", str, true ) end
+
+    reaper.UpdateArrange()
 end
 
-if reaper.CountSelectedMediaItems(0) > 0 then
-    Main(re_table)
-end
